123b33a935f4195d25ab20f68ab5aac9b2761688
GiacomanSalim <giacoman_salim@hotmail.com>
2024-09-15 01:16:48 -0600

    fixing conflict

--- models/subject_model.py
+++ models/subject_model.py
@@ -2,15 +2,7 @@ from pydantic import BaseModel
 import datetime
 
 class Subject(BaseModel):
-<<<<<<< HEAD
-<<<<<<< HEAD
-    id: int
-=======
     id: str | None = None
->>>>>>> c141412 (feat: :sparkles: added subjects create, read all and read one by id)
-=======
-    id: str | None = None
->>>>>>> upstream/zapataLuis
     name: str
     description: str
     units: list
