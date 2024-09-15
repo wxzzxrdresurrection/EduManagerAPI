ca300159b926de0047ebf82f8a32d3f05a9485f7
GiacomanSalim <giacoman_salim@hotmail.com>
2024-09-15 00:33:33 -0600

    done

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ load_dotenv()
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routers.subject import subject
 from routers.ws import ws_router
+from routers.subject import subjects
 
 app = FastAPI(
     title="EduManagerAPI",
@@ -19,6 +19,5 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
-app.include_router(subject, prefix="/subject", tags=["subject"])
+app.include_router(subjects, prefix="/subject", tags=["subject"])
 app.include_router(ws_router, prefix="/ws", tags=["ws"])
--- models/subject_model.py
+++ models/subject_model.py
@@ -2,10 +2,14 @@ from pydantic import BaseModel
 import datetime
 
 class Subject(BaseModel):
+<<<<<<< HEAD
     id: int
+=======
+    id: str | None = None
+>>>>>>> c141412 (feat: :sparkles: added subjects create, read all and read one by id)
     name: str
     description: str
     units: list
-    created_at: datetime.datetime
-    updated_at: datetime.datetime
-    deleted_at: datetime.datetime
+    created_at: datetime.datetime = datetime.datetime.now()
+    updated_at: datetime.datetime | None = None
+    deleted_at: datetime.datetime | None = None
